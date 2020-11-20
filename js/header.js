ee5e3707f3d75e7558e4dde81e4c258165c6e83c
Sanket Sinha <sinhasanket3@gmail.com>
2020-11-20 09:54:27 +0545

    Creation Of Second Page

---
+++ js/header.js
@@ -0,0 +1,82 @@
+            //Sign Up Button
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+
+
+            //Sign In button
+// Get the modal
+var modals = document.getElementById("myModals");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modals.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modals.style.display = "none";
+}
+
+
+          // create post button
+// Get the modal
+var cpBtn = document.getElementById("cpBtns");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  cpBtn.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  cpBtn.style.display = "none";
+}
+
+
+// Get the re-signup modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("resg");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var btn = document.getElementsByClassName("sgnbtn")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modals.style.display = "none";
+  modal.style.display = "block";
+}
+
